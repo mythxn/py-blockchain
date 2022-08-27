baca69a9496f3593871d98206eda6e56255c75c6
mythxn <mithun.shanoj@dubizzle.com>
2022-08-27 19:21:35 +0400

    wallet: minor stylistic updates

--- wallet.py
+++ wallet.py
@@ -18,23 +18,23 @@ class Wallet():
         return signature.hex()
 
     @staticmethod
-    def signature_valid(data, signature, public_key_string):
+    def signature_valid(data, signature, pub_key_string):
         signature = bytes.fromhex(signature)
         dataHash = ChainUtils.hash(data)
-        pub_key = RSA.import_key(public_key_string)
+        pub_key = RSA.import_key(pub_key_string)
         signature_scheme_obj = PKCS1_v1_5.new(pub_key)
         return signature_scheme_obj.verify(dataHash, signature)
 
-    def public_key_string(self):
+    def pub_key_string(self):
         return self.key_pair.publickey().export_key('PEM').decode('utf-8')
 
     def create_transaction(self, receiver_pub_key, amount, type):
-        transaction = Transaction(self.public_key_string(), receiver_pub_key, amount, type)
+        transaction = Transaction(self.pub_key_string(), receiver_pub_key, amount, type)
         signature = self.sign(transaction.payload())
         transaction.sign(signature)
         return transaction
 
     def create_block(self, transactions, prev_hash, block_count):
-        block = Block(transactions, prev_hash, self.public_key_string(), block_count)
+        block = Block(transactions, prev_hash, self.pub_key_string(), block_count)
         block.sign(self.sign(block.payload()))
         return block

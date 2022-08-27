37efc3c66990bbdff175ea855b09141a28bca4f0
mythxn <mithun.shanoj@dubizzle.com>
2022-08-27 14:56:25 +0400

    transaction: initial model

---
+++ transaction.py
@@ -0,0 +1,15 @@
+import time
+
+import uuid
+
+
+class Transaction():
+
+    def __init__(self, senderPubKey, receiverPubKey, amount, type):
+        self.sender = senderPubKey
+        self.recipient = receiverPubKey
+        self.amount = amount
+        self.type = type
+        self.id = uuid.uuid1().hex
+        self.timestamp = time.time()
+        self.signature = ''

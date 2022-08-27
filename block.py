e62dae7e7fed7b298969abae507ddfdded6d674a
mythxn <mithun.shanoj@dubizzle.com>
2022-08-27 17:05:29 +0400

    introduce `Block` class

---
+++ block.py
@@ -0,0 +1,24 @@
+import copy
+import time
+
+
+class Block():
+    def __init__(self, transactions, prev_hash, forger, block_count):
+        self.transactions = transactions
+        self.prev_hash = prev_hash
+        self.forger = forger
+        self.block_count = block_count
+        self.timestamp = time.time()
+        self.signature = ''
+
+    def to_json(self):
+        data = {
+            'prev_hash': self.prev_hash,
+            'forger': self.forger,
+            'block_count': self.block_count,
+            'timestamp': self.timestamp,
+            'signature': self.signature
+        }
+        json_transactions = [transaction.to_json() for transaction in self.transactions]
+        data['transactions'] = json_transactions
+        return data

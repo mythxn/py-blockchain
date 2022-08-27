f8a74434551ce62c14c157b8f4637ec2ead8e8c9
mythxn <mithun.shanoj@dubizzle.com>
2022-08-27 20:45:16 +0400

    transaction_pool: add `remove_from_pool` logic

--- transaction_pool.py
+++ transaction_pool.py
@@ -7,3 +7,12 @@ class TransactionPool():
 
     def transaction_exists(self, transaction):
         return transaction in self.transactions
+
+    def remove_from_pool(self, transactions):
+        new_pool_transactions = []
+
+        for cur_pool_transaction in self.transactions:
+            if cur_pool_transaction not in transactions:
+                new_pool_transactions.append(cur_pool_transaction)
+
+        self.transactions = new_pool_transactions

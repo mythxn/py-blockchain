f7ad653833cc89397a58db7ccd636d0b024935c6
mythxn <mithun.shanoj@dubizzle.com>
2022-08-27 15:25:40 +0400

    introduce `ChainUtils` class

---
+++ chain_utils.py
@@ -0,0 +1,10 @@
+from Crypto.Hash import SHA256
+import json
+
+class ChainUtils():
+
+    @staticmethod
+    def hash(data):
+        dataString = json.dumps(data)
+        dataBytes = dataString.encode('utf-8')
+        return SHA256.new(dataBytes)

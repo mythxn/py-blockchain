bb54313aaf07045c142a429a8a9398fc2f3d08cc
mythxn <mithun.shanoj@dubizzle.com>
2022-08-27 19:21:58 +0400

    main: test out `AccountModel`

--- main.py
+++ main.py
@@ -1,39 +1,11 @@
-from pprint import pprint
-
-from blockchain import Blockchain
-from chain_utils import ChainUtils
-from transaction_pool import TransactionPool
+from account_model import AccountModel
 from wallet import Wallet
 
 if __name__ == '__main__':
-    sender = 'sender'
-    receiver = 'receiver'
-    amount = 1
-    type = 'transfer'
 
     wallet = Wallet()
-    fraud_wallet = Wallet()
-    pool = TransactionPool()
+    account_model = AccountModel()
 
-    transaction = wallet.create_transaction(receiver, amount, type)
-
-    if not pool.transaction_exists(transaction):
-        pool.add_transaction(transaction)
-
-    blockchain = Blockchain()
-
-    prev_hash = ChainUtils.hash(blockchain.blocks[-1].payload()).hexdigest()
-    block_count = len(blockchain.blocks)
-
-    block = wallet.create_block(pool.transactions, prev_hash, block_count)
-
-    if not blockchain.prev_block_hash_valid(block):
-        print('Previous block hash is invalid')
-
-    if not blockchain.block_count_valid(block):
-        print('Block count is invalid')
-
-    if blockchain.prev_block_hash_valid(block) and blockchain.block_count_valid(block):
-        blockchain.add_block(block)
-
-    pprint(blockchain.to_json())
+    account_model.update_balance(wallet.pub_key_string(), 5)
+    account_model.update_balance(wallet.pub_key_string(), -15)
+    print(account_model.balances)

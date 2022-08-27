dc870836454720cdab47c46d86c8ec4acff5062d
mythxn <mithun.shanoj@dubizzle.com>
2022-08-27 20:35:52 +0400

    main: test out executing transactions on the chain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from pprint import pprint
+
+from block import Block
 from blockchain import Blockchain
+from chain_utils import ChainUtils
 from transaction_pool import TransactionPool
 from wallet import Wallet
 
@@ -10,10 +14,17 @@ if __name__ == '__main__':
     alice = Wallet()
     bob = Wallet()
     exchange = Wallet()
+    forger = Wallet()
 
     exchange_transaction = exchange.create_transaction(alice.pub_key_string(), 10, 'exchange')
     if not pool.transaction_exists(exchange_transaction):
         pool.add_transaction(exchange_transaction)
+
+    covered_transactions = blockchain.get_covered_trasaction_set(pool.transactions)
+    prev_hash = ChainUtils.hash(blockchain.blocks[-1].payload()).hexdigest()
+    block_count = len(blockchain.blocks)
+    block_one = Block(covered_transactions, prev_hash, forger.pub_key_string(), block_count)
+    blockchain.add_block(block_one)
 
     # alice wants to send 5 tokens to bob
     transaction = alice.create_transaction(bob.pub_key_string(), 5, 'transfer')
@@ -22,5 +33,9 @@ if __name__ == '__main__':
         pool.add_transaction(transaction)
 
     covered_transactions = blockchain.get_covered_trasaction_set(pool.transactions)
+    prev_hash = ChainUtils.hash(blockchain.blocks[-1].payload()).hexdigest()
+    block_count = len(blockchain.blocks)
+    block_two = Block(covered_transactions, prev_hash, forger.pub_key_string(), block_count)
+    blockchain.add_block(block_two)
 
-    print(covered_transactions)
+    pprint(blockchain.to_json())

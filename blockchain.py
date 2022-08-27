1504bac14548d204a1e8404a04f54e55bc26049e
mythxn <mithun.shanoj@dubizzle.com>
2022-08-27 19:54:49 +0400

    blockchain: integrate `AccountModel`

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,4 @@
+from account_model import AccountModel
 from block import Block
 from chain_utils import ChainUtils
 
@@ -5,6 +6,7 @@ from chain_utils import ChainUtils
 class Blockchain():
     def __init__(self):
         self.blocks = [Block.genesis()]
+        self.account_model = AccountModel()
 
     def add_block(self, block):
         self.blocks.append(block)
@@ -18,3 +20,16 @@ class Blockchain():
     def prev_block_hash_valid(self, block):
         latest_blockchain_hash = ChainUtils.hash(self.blocks[-1].payload()).hexdigest()
         return latest_blockchain_hash == block.prev_hash
+
+    def transaction_covered(self, transaction):
+        sender_bal = self.account_model.get_balance(transaction.sender_pub_key)
+        return sender_bal >= transaction.amount
+
+    def get_covered_trasaction_set(self, transactions):
+        covered_transactions = []
+        for transaction in transactions:
+            if self.transaction_covered(transaction):
+                covered_transactions.append(transaction)
+            else:
+                print('Transaction not covered')
+        return covered_transactions

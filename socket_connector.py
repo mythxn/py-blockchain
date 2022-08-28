9dd3940d6d750fdf085d6a25473828a1ba1ff252
mythxn <mithun.shanoj@dubizzle.com>
2022-08-28 11:43:38 +0400

    setup `socket_connector`

---
+++ socket_connector.py
@@ -0,0 +1,8 @@
+class SocketConnector():
+
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
+    def __eq__(self, other):
+        return self.ip == other.ip and self.port == other.port

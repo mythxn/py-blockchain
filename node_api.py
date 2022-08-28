9784ec6ba5612beb98383fce30dc0cfecebe35de
mythxn <mithun.shanoj@dubizzle.com>
2022-08-28 19:48:42 +0400

    node_api: implement `transaction` POST endpoint

--- node_api.py
+++ node_api.py
@@ -1,5 +1,7 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+from chain_utils import ChainUtils
 
 app = FastAPI()
 node = None
@@ -23,3 +25,12 @@ class NodeAPI:
     @app.get("/transaction_pool")
     async def transaction_pool():
         return {ctr: transaction.to_json() for ctr, transaction in enumerate(node.transaction_pool.transactions)}
+
+    @app.post("/transaction")
+    async def transaction(self: Request):
+        request_body = await self.json()
+        if 'transaction' not in request_body:
+            return {'message': 'No transaction in request'}
+        transaction = ChainUtils.decode(request_body['transaction'])
+        node.handle_transaction(transaction)
+        return {'message': 'Transaction added to pool'}

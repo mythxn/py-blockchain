df587d922ee0c7d0e0f5f4469e301cb3f3c7ddc3
mythxn <mithun.shanoj@dubizzle.com>
2022-08-28 11:35:26 +0400

    node: integrate `p2p` communication

--- node.py
+++ node.py
@@ -1,10 +1,18 @@
 from blockchain import Blockchain
+from socket_communication import SocketCommunication
 from transaction_pool import TransactionPool
 from wallet import Wallet
 
 
 class Node():
-    def __init__(self):
+    def __init__(self, ip, port):
+        self.p2p = None
+        self.ip = ip
+        self.port = port
         self.transaction_pool = TransactionPool()
         self.wallet = Wallet()
         self.blockchain = Blockchain()
+
+    def start_p2p(self):
+        self.p2p = SocketCommunication(self.ip, self.port)
+        self.p2p.start_socket()

67b9bc32e302c6541d38b07e1bc161c8671e4cac
mythxn <mithun.shanoj@dubizzle.com>
2022-08-28 15:18:52 +0400

    peer_discovery_handler: publish and connect to peers from first node

--- peer_discovery_handler.py
+++ peer_discovery_handler.py
@@ -1,6 +1,9 @@
 import threading
 import time
 
+from chain_utils import ChainUtils
+from message import Message
+
 
 class PeerDiscoveryHandler():
 
@@ -22,4 +25,12 @@ class PeerDiscoveryHandler():
             time.sleep(10)
 
     def handshake(self, connected_node):
-        self.socket_communication.send(connected_node, f'Hello from {self.socket_communication.port}')
+        self.socket_communication.send(connected_node, self.handshake_message())
+
+    def handshake_message(self):
+        own_connector = self.socket_communication.socket_connector
+        own_peers = self.socket_communication.peers
+        data = own_peers
+        message_type = 'discovery'
+        message = Message(own_connector, message_type, data)
+        return ChainUtils.encode(message)

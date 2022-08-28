7312443f136f5d27614b54b74fb2925eac37498c
mythxn <mithun.shanoj@dubizzle.com>
2022-08-28 11:35:10 +0400

    setup `socket_communication` for p2p communication

---
+++ socket_communication.py
@@ -0,0 +1,20 @@
+from p2pnetwork.node import Node
+
+
+class SocketCommunication(Node):
+    def __init__(self, ip, port):
+        super().__init__(ip, port, None)
+
+    def start_socket(self):
+        self.start()
+
+    def inbound_node_connected(self, connected_node):
+        print('inbound_node_connected')
+        self.send_to_node(connected_node, f'Hello from {self.port}')
+
+    def outbound_node_connected(self, connected_node):
+        print('outbound_node_connected')
+        self.send_to_node(connected_node, f'Hello from {self.port}')
+
+    def node_message(self, connected_node, message):
+        print(message)

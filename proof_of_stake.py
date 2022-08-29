c689c35245d0bb1eb3b6a82425719945eaf29210
mythxn <mithun.shanoj@dubizzle.com>
2022-08-29 13:40:24 +0400

    proof_of_stake: setup `forger` logic & luck
    -- also test it

--- proof_of_stake.py
+++ proof_of_stake.py
@@ -1,13 +1,40 @@
-class ProofOfStake:
+from chain_utils import ChainUtils
+from lot import Lot
+
+
+class ProofOfStake():
 
     def __init__(self):
         self.stakers = {}
 
     def update(self, pub_key_string, stake):
-        if pub_key_string in self.stakers:
+        if pub_key_string in self.stakers.keys():
             self.stakers[pub_key_string] += stake
         else:
             self.stakers[pub_key_string] = stake
 
     def get(self, pub_key_string):
         return self.stakers.get(pub_key_string, None)
+
+    def validator_lots(self, seed):
+        lots = []
+        for validator in self.stakers.keys():
+            lots.extend(Lot(validator, stake + 1, seed) for stake in range(self.get(validator)))
+        return lots
+
+    def winner_lot(self, lots, seed):
+        winner_lot = None
+        least_offset = None
+        reference_hash_int_value = int(ChainUtils.hash(seed).hexdigest(), 16)
+        for lot in lots:
+            lot_int_value = int(lot.lot_hash(), 16)
+            offset = abs(lot_int_value - reference_hash_int_value)
+            if least_offset is None or offset < least_offset:
+                least_offset = offset
+                winner_lot = lot
+        return winner_lot
+
+    def forger(self, last_blockHash):
+        lots = self.validator_lots(last_blockHash)
+        winner_lot = self.winner_lot(lots, last_blockHash)
+        return winner_lot.pub_key
--- test.py
+++ test.py
@@ -1,12 +1,26 @@
-from lot import Lot
+import random
+import string
+
+from proof_of_stake import ProofOfStake
+
+
+def get_random_string(length_of_str):
+    return ''.join(random.choice(string.ascii_letters) for _ in range(length_of_str))
+
 
 if __name__ == '__main__':
-    # pos = ProofOfStake()
-    # pos.update('alice', 10)
-    # pos.update('bob', 20)
-    #
-    # print(pos.get('alice'))
-    # print(pos.get('bob'))
-    # print(pos.get('carol'))
+    pos = ProofOfStake()
+    pos.update('alice', 100)
+    pos.update('bob', 100)
 
-    lot = Lot('alice', 1, 'prev_block_hash')
+    bob_wins = alice_wins = 0
+
+    for i in range(100):
+        forger = pos.forger(get_random_string(i))
+        if forger == 'alice':
+            alice_wins += 1
+        elif forger == 'bob':
+            bob_wins += 1
+
+    print(f'Alice wins {alice_wins} times')
+    print(f'Bob wins {bob_wins} times')

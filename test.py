f3d54ac440a1562d3a38669b3d1c3d1015b4ea4a
mythxn <mithun.shanoj@dubizzle.com>
2022-08-29 12:44:54 +0400

    test: test `Lot` class

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
-from proof_of_stake import ProofOfStake
+from lot import Lot
 
 if __name__ == '__main__':
-    pos = ProofOfStake()
-    pos.update('alice', 10)
-    pos.update('bob', 20)
+    # pos = ProofOfStake()
+    # pos.update('alice', 10)
+    # pos.update('bob', 20)
+    #
+    # print(pos.get('alice'))
+    # print(pos.get('bob'))
+    # print(pos.get('carol'))
 
-    print(pos.get('alice'))
-    print(pos.get('bob'))
-    print(pos.get('carol'))
+    lot = Lot('alice', 1, 'prev_block_hash')

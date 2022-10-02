ac1e1766ef3c8f63203d1c50e7b77493d7a39182
mythxn <mithun.shanoj@dubizzle.com>
2022-10-02 14:07:53 +0400

    final touches

--- interaction.py
+++ interaction.py
@@ -3,14 +3,23 @@ import requests
 from chain_utils import ChainUtils
 from wallet import Wallet
 
+
+def post_transaction(sender, recipient, amount, type):
+    transaction = sender.create_transaction(recipient.pub_key_string(), amount, type)
+    url = 'http://localhost:5000/transaction'
+    package = {'transaction': ChainUtils.encode(transaction)}
+    requests.post(url, json=package)
+
 if __name__ == '__main__':
     bob = Wallet()
     alice = Wallet()
+    alice.from_key('keys/stakerPrivateKey.pem')
     exchange = Wallet()
 
-    transaction = exchange.create_transaction(alice.pub_key_string(), 10, 'exchange')
+    # forger: genesis
+    post_transaction(exchange, alice, 100, 'exchange')
+    post_transaction(exchange, bob, 100, 'exchange')
+    post_transaction(exchange, alice, 25, 'stake')
 
-    url = 'http://localhost:5001/transaction'
-    payload = {'transaction': ChainUtils.encode(transaction)}
-    resp = requests.post(url, json=payload)
-    print(resp.text)
+    # forger: probably alice
+    post_transaction(alice, bob, 1, 'transfer')
